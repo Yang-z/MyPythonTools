d0c67deafae33d2d5c82d9638d7070ed73378536
Yang-z <zheng_yang_yang@msn.com>
2019-10-25 14:52:23 +0800

    optimize "timestamp_helper.py"

--- timestamp_helper.py
+++ timestamp_helper.py
@@ -2,25 +2,45 @@ import os
 
 
 class TimestampHelper(object):
-
     @staticmethod
     def show(path):
-        print("atime:", os.path.getatime(path))
+        print(path)
+        # print("atime:", os.path.getatime(path))
         print("mtime:", os.path.getmtime(path))
 
     @staticmethod
-    def copy(to_path, from_path):
-        os.utime(to_path, (
-            os.path.getatime(from_path),
-            os.path.getmtime(from_path),
-        ))
-        TimestampHelper.show(to_path)
+    def set(path, timestamp, sub=False):
+        timestamp_o = os.path.getmtime(path)
+
+        # only bring time ahead，no delay
+        if timestamp < timestamp_o:
+            os.utime(path, (timestamp, timestamp))
+            TimestampHelper.show(path)
+
+        # do for the sub path
+        if sub and os.path.isdir(path):
+            for f in os.listdir(path):
+                sub_path = os.path.join(path, f)
+                TimestampHelper.set(sub_path, timestamp, sub)
 
     @staticmethod
-    def shift(path, sec):
-        ts = os.path.getmtime(path)
-        ts += sec
+    def copy(target_path, source_path, sub=False):
+        source_mt = os.path.getmtime(source_path)
+        TimestampHelper.set(target_path, source_mt, sub)
+
+    @staticmethod
+    def shift(path, shift_sec, sub=False):
+        ts = os.path.getmtime(path) + shift_sec
         os.utime(path, (ts, ts))
+        TimestampHelper.show(path)
+
+        # do for the sub path
+        if sub and os.path.isdir(path):
+            for f in os.listdir(path):
+                sub_path = os.path.join(path, f)
+                TimestampHelper.shift(sub_path, shift_sec, sub)
+
+
 
 
 if __name__ == "__main__":
@@ -30,5 +50,6 @@ if __name__ == "__main__":
     # filedialog.askdirectory()
 
     # TimestampHelper.show(filedialog.askopenfilename())
-    # TimestampHelper.copy(filedialog.askopenfilename(), filedialog.askopenfilename())
-    # TimestampHelper.shift(filedialog.askdirectory(), -365*24*60*60)
+    # TimestampHelper.set(filedialog.askopenfilename(), float(input('timestamp: ')), False)
+    # TimestampHelper.copy(filedialog.askopenfilename(), filedialog.askdirectory(), False)
+    # TimestampHelper.shift(filedialog.askdirectory(), -1*365*24*60*60, True)

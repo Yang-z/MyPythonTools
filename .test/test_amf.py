065cc008d4f1bd5ed811564cd774902567056aa8
Yang-z <zheng_yang_yang@msn.com>
2020-02-26 03:55:20 +0800

    rename test files to avoid name conflicts with build-in modules

--- .test/test_amf.py
+++ .test/test_amf.py
@@ -5,5 +5,9 @@ from pyamf import remoting as pyamf_remoting
 
 with open(filedialog.askopenfilename(), 'rb') as f:
     content: bytes = f.read()
-    decoded = pyamf_remoting.decode(content)
-    print(decoded)
+    try:
+        decoded = pyamf_remoting.decode(content)
+    except Exception as e:
+        print(e)
+
+    print("done!")
